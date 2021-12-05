5ec1da4b46673f0b0643b440bfbf9b17a9d93e05
hyunseo <hansx2079@naver.com>
2021-12-05 23:49:36 +0900

    기초 세팅1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrap>
+      <div>
+        Test
+      </div>
+    </Wrap>
   );
 }
+
+const Wrap = styled.div`
+  width: 380px;
+  height: 844px;
+  margin: 0 auto;
+  box-shadow: 0 1px 10px rgb(0 0 0 / 20%);
+  overflow: hidden;
+`;
 
 export default App;
