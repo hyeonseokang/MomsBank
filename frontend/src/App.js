6f1ced38e1a00c77901803e237b0fe663100d2c6
hyunseo <hansx2079@naver.com>
2021-12-08 22:06:29 +0900

    Test 하던거 정리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,13 @@
 import styled from 'styled-components';
 import Main from './components/main';
-import rootReducer, {rootSaga} from './modules';
-import createSagaMiddleware from 'redux-saga';
-import { Provider } from 'react-redux';
-import {createStore, applyMiddleware}from 'redux';
 
-const sagaMiddleWare = createSagaMiddleware();
-const store = createStore(
-  rootReducer,
-  applyMiddleware(sagaMiddleWare)
-);
-
-sagaMiddleWare.run(rootSaga);
 function App() {
   return (
-    <Provider store = {store}>
-  <Wrap>
+      <Wrap>
         <Main/>
       </Wrap>
-    </Provider>
   );
 }
-
-const Test = styled.div`
-  display: inline-block;
-  width: 100px;
-  height: 100px;
-  background-color: white;
-  border: 1px solid #0000ff;
-`;
-
-const Test2 = styled.div`
-  display: inline-block;
-  width: 100px;
-  height: 100px;
-  background-color: white;
-  border: 1px solid #0000ff;
-  >p{
-    float: top;
-    margin-left: 20px;
-  }
-`;
 
 const Wrap = styled.div`
   width: 380px;
--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -5,36 +5,9 @@ import Title from '../Title'
 import RepayDisplay from './RepayDisplay'
 import MenuBlock from '../MenuBlock';
 
-import {useSelector, useDispatch} from 'react-redux';
-import {getTransfers, addTransfer} from '../../modules/transfer';
-import { useEffect } from 'react';
-import { addDeposit, getDeposits } from '../../modules/deposit';
-
 const Main = () => {
-    
-  const {data, loading, error} = useSelector(state => state.deposits);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getDeposits());
-  }, [dispatch]);
-
-  if (loading == true){
-    console.log(data);
-  }
-
     return (
-        <Div onClick = {
-          () => {
-            dispatch(addDeposit({
-              "name": "김철수",
-              "bank_name": "케이뱅크",
-              "account_number": "123412343333",
-              "amount": 1111,
-              "deadline": "2021-12-15"
-            }));
-          }
-        }>
+        <Div>
             <Title></Title>
             <MenuBlockDiv>
                 <RepayDisplay/>
