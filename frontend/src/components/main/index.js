7004bedbd9b3e58894075301ef4c8c0444573482
hyunseo <hansx2079@naver.com>
2021-12-09 12:35:12 +0900

    Main 페이지 쪽 조금

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -9,6 +9,8 @@ import Title from '../Title'
 import RepayDisplay from './RepayDisplay'
 import MenuBlock from '../MenuBlock';
 
+import moneyIcon from '../../images/icon/check.svg';
+import nextIcon from '../../images/icon/next.svg'
 import getDepositDDay from '../../utils/DepositDate';
 
 const Main = () => {
@@ -37,7 +39,13 @@ const Main = () => {
             <Title></Title>
             <MenuBlockDiv>
                 <RepayDisplay dDay={dDay}/>
-                <MenuBlock height = "68px"/>
+                <MenuBlock height = "68px">
+                  <MoneyIcon>
+                    <img src={moneyIcon}/>
+                  </MoneyIcon>
+                  <Text>조회</Text>
+                  <NextIcon src={nextIcon}/>
+                </MenuBlock>
                 <MenuBlock height = "427px"/>
             </MenuBlockDiv>
         </Div>
@@ -45,12 +53,48 @@ const Main = () => {
 };
 
 const Div = styled.div`
+
 `;
 
 const MenuBlockDiv = styled.div`
-    >div{
-        margin-bottom: 8px;
-    }
+  >div{
+      margin-bottom: 8px;
+  }
+`;
+
+const MoneyIcon = styled.div`
+  display: inline-block;
+  >img{
+    margin-top: 10px;
+    margin-bottom: 10px;
+    margin-left: 15px;
+    width: 48px;
+    height: 48px;
+  }
+`;
+
+const NextIcon = styled.img`
+  width: 48px;
+  height: 48px;
+  float: right;
+  margin:auto;
+`;
+
+const Text = styled.div`
+  display:inline-block;
+  width:63px;
+  height: 72px;
+
+  margin-top:21px;
+  margin-left: 93px;
+  
+  vertical-align: top;
+  font-family: Spoqa Han Sans Neo;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 25px;
+
+  color: #2F69FF;
 `;
 
 export default Main;
--- frontend/src/utils/DepositDate.js
+++ frontend/src/utils/DepositDate.js
@@ -1,28 +1,32 @@
 const getDepositDDay = (deposits) => {
-    if (deposits.length === 0)
+    try {
+        if (deposits.length === 0)
         return -1;
 
-    const {deadline} = deposits[0];
-    const nowDate = new Date();
-    const endDate = new Date(deadline);
-    endDate.setDate(endDate.getDate()+1)
-    endDate.setHours(0);
-    endDate.setSeconds(0);
-    
-    let dayDifference = endDate.getTime() - nowDate.getTime();
-    const dDay = dayDifference / 86400000; // 1000 * 60 * 60 * 24
+        const {deadline} = deposits[0];
+        const nowDate = new Date();
+        const endDate = new Date(deadline);
+        endDate.setDate(endDate.getDate()+1)
+        endDate.setHours(0);
+        endDate.setSeconds(0);
+        
+        let dayDifference = endDate.getTime() - nowDate.getTime();
+        const dDay = dayDifference / 86400000; // 1000 * 60 * 60 * 24
 
-    if (dDay <= 1.0){
-        dayDifference = dayDifference / 1000;
-        const hour = parseInt(dayDifference / 3600); // 60 * 60
-        dayDifference = dayDifference % 3600;
-        const minute = parseInt(dayDifference / 60);
-        const second = parseInt(dayDifference % 60);
+        if (dDay <= 1.0){
+            dayDifference = dayDifference / 1000;
+            const hour = parseInt(dayDifference / 3600); // 60 * 60
+            dayDifference = dayDifference % 3600;
+            const minute = parseInt(dayDifference / 60);
+            const second = parseInt(dayDifference % 60);
 
-        return `${hour}시간 ${minute}분 ${second}초`;
+            return `${hour}시간 ${minute}분 ${second}초`;
+        }
+
+        return parseInt(dDay) + '일';
+    } catch (e) {
+        return -1;
     }
-
-    return parseInt(dDay) + '일';
 };
 
 export default getDepositDDay;
